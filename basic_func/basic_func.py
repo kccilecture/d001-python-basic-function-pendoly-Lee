f02d001448d59303deb1c931882955a11b3d5237
litcoder <pendoly@gmail.com>
2026-01-07 11:43:51 +0900

    과제받으세요

--- basic_func/basic_func.py
+++ basic_func/basic_func.py
@@ -19,14 +19,14 @@ def power(base, pow):
 
 
 def square(base):
-    return base ** (1/2)
+    return base ** 2
 
 
 def greet(이름="낯선자", 나이=20):
-    if 나이 > 40:
-        print(f"안녕하십니까 {이름}!")
-    elif 나이 > 20:
-        print("f안녕하신가 {이름}!")
+    if 나이 >= 40:
+        return f"안녕하십니까 {이름}!"
+    elif 나이 >= 20:
+        return f"안녕하신가 {이름}!"
     else :
-        print("f안녕 {이름}!")
+        return f"안녕 {이름}!"
 
